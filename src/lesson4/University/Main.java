776b57cb670ef45d39f3e5ed0753a6cd6a17f10f
Василий <112932239+MarDendu@users.noreply.github.com>
2022-09-27 23:12:37 +0300

    1

--- src/lesson4/University/Main.java
+++ src/lesson4/University/Main.java
@@ -1,17 +1,37 @@
 package lesson4.University;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Main {
 
-public class Main  {
-    public static void main(String[] args) {
-        Shape[] shapes = new Shape[3];
-        Circle circle1=new Circle("Черный",8,9,40);
-        Rectangle rectangle=new Rectangle("Черный",6,7,8,9);
-        Circle circle2=new Circle("",8,9,40);
-        shapes[0] =circle1;
-        shapes[1] = rectangle;
-        shapes[2] =circle2;
-        for(Shape shape: shapes){
-            shape.draw();
-        }
-        System.out.println(circle1.equals(circle2));
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("matrica.txt");
+
+        Matrix a = new Matrix(4, 4);
+        a.fillRandom();
+        a.print("A (Случайно заполненная матрица)");
+        a.toFile(fw, "A (Случайно заполненная матрица)");
+
+        Matrix b = new Matrix (new double[][]{{3, 2, 1, 1}, {1, 1, 2, 3}, {3, 2, 1, 1},{1, 1, 2, 3}});
+        b.print("B (Заполненаная вручная матрица");
+        b.toFile(fw, "B (Заполненаная вручная матрица)");
+
+        Matrix c = a.add(b);
+        c.print("C = A + B");
+        c.toFile(fw, "C = A + B");
+
+        Matrix d = c.multiply(2);
+        d.print("D = C * 2");
+        d.toFile(fw, "D = C * 2");
+
+        Matrix m = c.divide(2);
+        m.print("M = C / 2");
+        m.toFile(fw, "M = C / 2");
+
+        Matrix e = c.multiply(d);
+        e.print("E = C * D");
+        e.toFile(fw, "E = C * D");
+
+        fw.close();
+
     }
 }
